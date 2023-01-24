53b24d522a87415f2c104deb7b7250207ed1cbf2
Artanis <fabrizio+git@fabriziotobia.it>
2023-01-24 23:44:18 +0100

    Created a Game World scene, with a shader background and the initial steps of the player scene.

---
+++ source/Player/Player.gd
@@ -0,0 +1,88 @@
+extends Area2D
+
+export(String, "Direct", "RTS") var movement_mode
+export var rotation_speed: float = PI
+
+onready var target = position
+
+# Player velocity vector
+var velocity = Vector2()
+
+# Player Stats
+var speed = 100
+
+# DEBUGGING
+const max_zoom_out = 10
+
+func _physics_process(delta):
+	get_input()
+	
+	if (movement_mode == "Direct"):		
+		position += velocity * delta
+	elif (movement_mode == "RTS"):
+		velocity = position.direction_to(target) * speed
+		
+		## SMOOTH LOOKAT
+		# vector from the ship to the target
+		var v = target - self.global_position
+		
+		# get the angle of that vector
+		var angle = v.angle()
+		var r = global_rotation
+		
+		# ger toation allowed this frame
+		var angle_delta = rotation_speed * delta
+		
+		#get complete rotation to target
+		angle = lerp_angle(r, angle, 1.0)
+		
+		# limit that rotaiton to what is allowed this frame
+		angle = clamp(angle, r - angle_delta, r + angle_delta)
+		
+		# set ship rotation
+		global_rotation = angle		
+				
+		# Move only if the target is at least 5 units away and after the rotation to look at has finished		
+		if position.distance_to(target) > 5 and rotation == angle:
+			position += velocity * delta
+
+func get_input():
+	if (movement_mode == "Direct"):
+		velocity = Vector2()
+		
+		if Input.is_action_pressed("ui_right"):
+			velocity.x += 1
+		
+		if Input.is_action_pressed("ui_left"):
+			velocity.x -= 1
+		
+		if Input.is_action_pressed("ui_down"):
+			velocity.y += 1
+		
+		if Input.is_action_pressed("ui_up"):
+			velocity.y -= 1	
+			
+		if velocity.length() > 0:
+			velocity = velocity.normalized() * speed
+	elif (movement_mode == "RTS"):
+		if Input.is_action_just_released("left_click"):
+			target = get_global_mouse_position()			
+		
+	if Input.is_action_just_released("zoom_out"):		
+		#mouse_wheel down, zoom out
+		var cur_zoom = $Camera2D.get_zoom()
+		
+		if cur_zoom.x < max_zoom_out:
+			 #we're not too much zoomed out
+			cur_zoom = Vector2(cur_zoom.x + 1, cur_zoom.y + 1)
+			$Camera2D.set_zoom(cur_zoom)
+			
+	if Input.is_action_just_released("zoom_in"):		
+		#mouse_wheel up, zoom in
+		var cur_zoom = $Camera2D.get_zoom()		
+		
+		if cur_zoom.x > 1:						
+			 #we're not too much zoomed in
+			cur_zoom = Vector2(cur_zoom.x - 1, cur_zoom.y - 1)
+			$Camera2D.set_zoom(cur_zoom)			
+	

2c518431e8e1a606e33001f9f24925f0a4176e5e
Artanis <fabrizio+git@fabriziotobia.it>
2023-01-25 18:05:39 +0100

    Added a GameMusicPlayer to the GameWorld and BelowTheAsteroid theme.

---
+++ source/Helpers/GameMusicPlayer.gd
@@ -0,0 +1,12 @@
+extends Node
+
+# References to the nodes in our scene
+onready var _anim_player := $AnimationPlayer
+onready var _below_the_asteroids := $BelowTheAsteroids
+export(String, "BelowTheAsteroids") var starting_theme
+
+func _ready() -> void:
+	# at the beginnig, play...
+	match starting_theme:
+		"BelowTheAsteroids":
+			$BelowTheAsteroids.play()

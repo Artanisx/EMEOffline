198763dda0bb9325567e504bc447ca79b0327b5d
Artanis <fabrizio+git@fabriziotobia.it>
2023-01-26 11:42:31 +0100

    Added setup of player stats from save.

--- source/Main/GameWorld.gd
+++ source/Main/GameWorld.gd
@@ -2,4 +2,8 @@ extends Node2D
 
 onready var _player = $Player
 
-
+func _ready() -> void:
+	# Loads player variables
+	_player.SetPlayer(Globals.get_account_credits(), Globals.get_account_mininglaser(), Globals.get_account_cargoextender(), Globals.get_account_cargohold(), Globals.get_account_position())	
+	
+	
--- source/Player/Player.gd
+++ source/Player/Player.gd
@@ -1,20 +1,29 @@
 extends Area2D
 
-export(String, "Direct", "RTS", "Turtle") var movement_mode
+export(String, "Direct", "RTS", "Turtle", "Missile") var movement_mode
 export var rotation_speed: float = PI
 export var speed: float = 100
 export var dead_zone: int = 5
-
 onready var target = position
 onready var _space_dust = $Camera2D/SpaceDust			
 
+# PLAYER ACCOUNT STATS
+enum MINING_LASER {MINING_LASER_I = 1, MINING_LASER_II = 2, MINING_LASER_III = 3, MINING_LASER_IV = 4, MINING_LASER_V = 5}
+enum CARGO_EXTENDER {NO_CARGO_EXTENDER = 0, CARGO_EXTENDER_I = 1, CARGO_EXTENDER_II = 2, CARGO_EXTENDER_III = 3, CARGO_EXTENDER_IV = 4, CARGO_EXTENDER_V = 5}
+
+var player_credits: int = 0
+var player_mining_laser: int = 0
+var player_cargo_extender: int = 0
+var player_cargo_hold: int = 0
+
 # Player velocity vector
 var velocity := Vector2.ZERO
+var first_target_set = false
 
 # TurtleExample
-const ARRIVE_DISTANCE := 50.0
-const ARRIVED_THRESHOLD := 1.0
-var max_speed := 1000.0
+const ARRIVE_DISTANCE := 500.0
+const ARRIVED_THRESHOLD := 100.0
+var max_speed := 1500.0
 var target_mouse_position = Vector2.ZERO
 
 # DEBUGGING
@@ -36,9 +45,8 @@ func _process(delta):
 			desired_velocity *= distance_to_mouse / ARRIVE_DISTANCE
 		
 		var steering = desired_velocity - velocity
-		velocity += steering / 32.0	
+		velocity += steering / 32.0			
 		
-		velocity = lerp(velocity, velocity * max_speed,  0.1)	
 		position += velocity * delta
 		rotation = velocity.angle()
 		print(velocity.length())
@@ -50,7 +58,7 @@ func _physics_process(delta):
 	
 	if (movement_mode == "Direct"):		
 		position += velocity * delta
-	elif (movement_mode == "RTS"):
+	elif (movement_mode == "RTS" and first_target_set == true):
 		velocity = position.direction_to(target) * speed
 		
 		## SMOOTH LOOKAT
@@ -83,6 +91,14 @@ func _physics_process(delta):
 			_space_dust.direction("Right")
 			
 		print(velocity.length())
+	elif (movement_mode == "Missile"):
+		var accelerat = position.direction_to(target) * speed
+		velocity += accelerat * delta
+		velocity = velocity.clamped(speed)
+		rotation = velocity.angle()
+		if position.distance_to(target) > dead_zone:
+			position += velocity * delta
+		print(velocity.length())
 
 func get_input():
 	if (movement_mode == "Direct"):
@@ -102,9 +118,10 @@ func get_input():
 			
 		if velocity.length() > 0:
 			velocity = velocity.normalized() * speed
-	elif (movement_mode == "RTS"):
+	elif (movement_mode == "RTS" or movement_mode == "Missile"):
 		if Input.is_action_just_released("left_click"):
-			target = get_global_mouse_position()			
+			target = get_global_mouse_position()
+			first_target_set = true #allows the player to be moved. It needs to start false before any input or the player starting position would be accepted as a target to move
 		
 	if Input.is_action_just_released("zoom_out"):		
 		#mouse_wheel down, zoom out
@@ -124,3 +141,35 @@ func get_input():
 			cur_zoom = Vector2(cur_zoom.x - 1, cur_zoom.y - 1)
 			$Camera2D.set_zoom(cur_zoom)			
 	
+
+func SetPlayer(cred: int, mining: int, cargoxt: int, cargoh: int, pos: Vector2) -> void:
+	player_credits = cred
+	
+	match mining:
+		1:
+			player_mining_laser = MINING_LASER.MINING_LASER_I
+		2:
+			player_mining_laser = MINING_LASER.MINING_LASER_II
+		3:
+			player_mining_laser = MINING_LASER.MINING_LASER_III
+		4:
+			player_mining_laser = MINING_LASER.MINING_LASER_IV
+		5:
+			player_mining_laser = MINING_LASER.MINING_LASER_V
+			
+	match cargoxt:
+		0:
+			player_cargo_extender = CARGO_EXTENDER.NO_CARGO_EXTENDER
+		1:
+			player_cargo_extender = CARGO_EXTENDER.CARGO_EXTENDER_I
+		2:
+			player_cargo_extender = CARGO_EXTENDER.CARGO_EXTENDER_II
+		3:
+			player_cargo_extender = CARGO_EXTENDER.CARGO_EXTENDER_III
+		4:
+			player_cargo_extender = CARGO_EXTENDER.CARGO_EXTENDER_IV
+		5:
+			player_cargo_extender = CARGO_EXTENDER.CARGO_EXTENDER_V
+			
+	player_cargo_hold = cargoh
+	position = pos

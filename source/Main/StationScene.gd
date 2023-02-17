f1fafb0ac6770cdb9582c5b4c4f9f94106318f73
Artanis <fabrizio+git@fabriziotobia.it>
2023-02-17 11:54:39 +0100

    Already possible to undock, coming back to the game world.

--- source/Main/StationScene.gd
+++ source/Main/StationScene.gd
@@ -1,4 +1,5 @@
 extends ColorRect
+onready var scene_transition: ColorRect = $SceneTransition
 
 ## The animation speed
 export var animation_speed: float = 0.01
@@ -9,3 +10,12 @@ func _process(delta: float) -> void:
 	timer = timer + animation_speed
 	
 	self.material.set_shader_param("iTime", timer)		
+	
+func _unhandled_input(event: InputEvent) -> void:	
+	# Undock by clicking escape
+	if Input.is_action_just_released("ui_cancel"):
+		undock()
+	
+func undock() -> void:
+	# Undock for now simply loads back the game world scene
+	scene_transition.transition_to("res://source/Main/GameWorld.tscn")
